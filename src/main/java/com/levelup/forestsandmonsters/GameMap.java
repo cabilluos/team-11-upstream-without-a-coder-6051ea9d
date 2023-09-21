c3077a536b8bb3e9f63f8ff269894458029d52ad
Daniel Thompson <cabilluos@gmail.com>
2023-09-21 20:04:03 +0000

    Adding GameMapTest and GameMap implementation

---
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -0,0 +1,86 @@
+package com.levelup.forestsandmonsters;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
+public class GameMap {
+
+    private int startXCoordinate = 1;
+    private int startYCoordinate = 1;
+
+    private int xDimension = 10;
+    private int yDimension = 10;
+
+    private Position positions[][];
+
+    public GameMap(int xDimension, int yDimension) {
+        // result positions
+        this.positions = new Position[xDimension][yDimension];
+
+        // initialize x positions
+        for (int x=0 ; x < xDimension ; x++) {
+            // initialize y positions
+            for (int y=0 ; y < yDimension ; y++) {
+                positions[x][y] = new Position(x, y);
+            }
+        }
+
+        // initializing x dimesion
+        this.xDimension = xDimension;
+        // initializing y dimesion
+        this.yDimension = yDimension;
+    }
+
+    public Position[][] getPositions() {
+        return this.positions;
+    }
+
+    public Position calculatePosition(Position starPosition, DIRECTION direction) {
+
+        // set coordinates
+        int xCoordinates = starPosition.coordinates.x;
+        int yCoordinates = starPosition.coordinates.y;
+
+        // calculate position to the north
+        if (DIRECTION.NORTH.equals(direction)/* && this.startYCoordinate != yCoordinates */) {
+            yCoordinates--;
+        }
+
+        // calculate position to the south
+        if (DIRECTION.SOUTH.equals(direction)/* && yDimension != yCoordinates */) {
+            yCoordinates++;
+        }
+
+        // calculate position to the west
+        if (DIRECTION.WEST.equals(direction)/*&& this.startXCoordinate != xCoordinates*/) {
+            xCoordinates--;
+        }
+
+        // calculate position to the east
+        if (DIRECTION.EAST.equals(direction)/* && xDimension != xCoordinates */) {
+            xCoordinates++;
+        }
+
+        return new Position(xCoordinates, yCoordinates);
+    }
+
+    public boolean isPositionValid(Position positionCoordinates) {
+        boolean result = true;
+
+        if (positionCoordinates.coordinates.y < startYCoordinate) {
+            result = false;
+        } else if (positionCoordinates.coordinates.y >= yDimension ) {
+            result = false;
+        } else if (positionCoordinates.coordinates.x < startXCoordinate ) {
+            result = false;
+        } else if (positionCoordinates.coordinates.x >= xDimension ) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public int getTotalPositions() {
+        return this.xDimension * this.yDimension;
+    }
+    
+}
---
+++ src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
@@ -0,0 +1,134 @@
+package com.levelup.forestsandmonsters;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
+public class GameMapTest {
+
+    private GameMap map;
+    private int mapXDimension = 10;
+    private int mapYDimension = 10;
+
+    @Before
+    public void init() {
+        this.map = new GameMap(this.mapXDimension, this.mapYDimension);
+    } 
+
+    @Test
+    public void initializationGameMapTest() {
+        assertEquals(this.mapXDimension, this.map.getPositions().length);
+        assertEquals(this.mapYDimension, this.map.getPositions()[0].length);        
+    }
+
+    ///////////////////////////////
+    // non-bonce calculate position tests
+    @Test 
+    public void calculatePositionNonBonceNorthTest() {
+        // selecting a valid position x= 5 and y = 5 valid position
+        Position startingPosition = new Position(5, 5);
+        DIRECTION direction = DIRECTION.NORTH;
+
+        // checking if the final position is the right one
+        Position calculatedPosition = this.map.calculatePosition(startingPosition, direction);
+
+        // testing x axis
+        assertEquals(startingPosition.coordinates.x, calculatedPosition.coordinates.x);
+
+        // testing y axis
+        assertEquals(new Position(5, 4).coordinates.y, calculatedPosition.coordinates.y);
+    }
+
+    @Test 
+    public void calculatePositionNonBonceSouthTest() {
+        // selecting a valid position x= 5 and y = 5 valid position
+        Position startingPosition = new Position(5, 5);
+        DIRECTION direction = DIRECTION.SOUTH;
+
+        // checking if the final position is the right one
+        Position calculatedPosition = this.map.calculatePosition(startingPosition, direction);
+
+        // testing x axis
+        assertEquals(startingPosition.coordinates.x, calculatedPosition.coordinates.x);
+
+        // testing y axis
+        assertEquals(new Position(5, 6).coordinates.y, calculatedPosition.coordinates.y);
+    }
+
+    @Test 
+    public void calculatePositionNonBonceLeftTest() {
+        // selecting a valid position x= 5 and y = 5 valid position
+        Position startingPosition = new Position(5, 5);
+        DIRECTION direction = DIRECTION.WEST;
+
+        // checking if the final position is the right one
+        Position calculatedPosition = this.map.calculatePosition(startingPosition, direction);
+        Position targetPosition = new Position(4, 5);
+
+        // testing x axis
+        assertEquals(targetPosition.coordinates.x, calculatedPosition.coordinates.x);
+
+        // testing y axis
+        assertEquals(targetPosition.coordinates.y, calculatedPosition.coordinates.y);        
+    }
+
+    @Test 
+    public void calculatePositionNonBonceRightTest() {
+        // selecting a valid position x= 5 and y = 5 valid position
+        Position startingPosition = new Position(5, 5);
+        DIRECTION direction = DIRECTION.EAST;
+
+        // checking if the final position is the right one
+        Position calculatedPosition = this.map.calculatePosition(startingPosition, direction);
+        Position targetPosition = new Position(6, 5);
+
+        // testing x axis
+        assertEquals(targetPosition.coordinates.x, calculatedPosition.coordinates.x);
+
+        // testing y axis
+        assertEquals(targetPosition.coordinates.y, calculatedPosition.coordinates.y);
+    }
+
+    @Test 
+    public void shouldBeValidIsPositionValidTest() {
+        Position targetPosition = new Position(1, 1);
+        assertTrue(this.map.isPositionValid(targetPosition));
+    }
+
+    @Test 
+    public void shouldFailIsPositionValidBonceNorthTest() {
+        Position targetPosition = new Position(1, 0);
+        assertFalse(this.map.isPositionValid(targetPosition));
+    }
+
+    @Test 
+    public void shouldFailIsPositionValidBonceSouthTest() {
+        Position targetPosition = new Position(10, 11);
+        assertFalse(this.map.isPositionValid(targetPosition));
+    }
+
+    @Test 
+    public void shouldFailIsPositionValidBonceLeftTest() {
+        Position targetPosition = new Position(0, 1);
+        assertFalse(this.map.isPositionValid(targetPosition));
+    }
+    
+    @Test 
+    public void shouldFailIsPositionValidBonceRightTest() {
+        Position targetPosition = new Position(11, 1);
+        assertFalse(this.map.isPositionValid(targetPosition));
+    } 
+    
+    @Test 
+    public void validateGetTotalPositions() {        
+        int totalPosition = mapXDimension * mapYDimension;
+        assertEquals(totalPosition, this.map.getTotalPositions());
+    }  
+
+}
